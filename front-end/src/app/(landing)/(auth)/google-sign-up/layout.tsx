5b534b386c0f0f7eebc02ffc5dbbfab65bc96070
Peter Li <petercyli02@gmail.com>
2025-06-15 22:29:19 +0100

    new layout

---
+++ front-end/src/app/(landing)/(auth)/google-sign-up/layout.tsx
@@ -0,0 +1,16 @@
+import "../globals.css";
+import { Suspense } from "react";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <Suspense fallback={<div className="w-full text-center">Loading...</div>}>
+      {children}
+    </Suspense>
+  );
+}
+
+    
