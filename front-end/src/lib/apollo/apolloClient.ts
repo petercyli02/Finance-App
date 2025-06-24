ef67ecdca5b22f793f4d0fad3fa4f4e1aff3e6a0
Peter Li <petercyli02@gmail.com>
2025-06-25 00:26:28 +0100

    hard code server url

--- front-end/src/lib/apollo/apolloClient.ts
+++ front-end/src/lib/apollo/apolloClient.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 function createApolloClient() {
   const client = new ApolloClient({
     link: new HttpLink({
-      uri: `${process.env.NEXT_SERVER_URL}graphql`,
+      uri: `http://finapp.us-east-1.elasticbeanstalk.com/graphql`,
     }),
     cache: new InMemoryCache(),
   });
