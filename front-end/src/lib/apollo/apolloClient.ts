e2fe56fc5000e6f45a867eabb06aacddeff4fc13
Peter Li <petercyli02@gmail.com>
2025-06-25 01:27:50 +0100

    changed hard code server url to https

--- front-end/src/lib/apollo/apolloClient.ts
+++ front-end/src/lib/apollo/apolloClient.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 function createApolloClient() {
   const client = new ApolloClient({
     link: new HttpLink({
-      uri: `http://finapp.us-east-1.elasticbeanstalk.com/graphql`,
+      uri: `https://finapp.us-east-1.elasticbeanstalk.com/graphql`,
     }),
     cache: new InMemoryCache(),
   });
